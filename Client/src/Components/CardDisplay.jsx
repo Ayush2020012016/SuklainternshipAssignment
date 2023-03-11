461ba9aab294078b40e4933d86e6cc7b71f4d2dd
Ayush20200012016 <ayush2020012016@tulas.edu.in>
2023-03-11 16:33:01 +0530

    d

--- Client/src/Components/CardDisplay.jsx
+++ Client/src/Components/CardDisplay.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Card from "./Cads/CardUI";
 
-const CardDisplay = () => {
-  return <div>CardDisplay</div>;
+const CardDisplay = ({ category }) => {
+  console.log(category);
+  const [Carddata, setCardData] = useState([]);
+
+  useEffect(() => {
+    const getCards = async () => {
+      const data = await fetch("http://localhost:4000/cards");
+      const unfilteredCardData = await data.json();
+
+      setCardData(unfilteredCardData);
+    };
+    getCards();
+    filterData();
+  }, []);
+
+  const filterData = () => {
+    const filteredCardData = Carddata.filter(
+      (value) => value.category === category
+    );
+    setCardData(filteredCardData);
+  };
+
+  console.log(Carddata);
+  return (
+    <div className="grid grid-cols-4 gap-2">
+      {Carddata.map((value, index) => {
+        return (
+          <div key={index} className="">
+            <Card />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CardDisplay;
